9b175418d831a018db193333b8eaa824ce4f3f04
dskprt <59660013+dskprt@users.noreply.github.com>
2020-03-24 18:12:58 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(name="fastcord",
+    version="0.0.1",
+    description="another discord api wrapper for writing bots",
+    author="dskprt",
+    url="https://github.com/dskprt/fastcord",
+    packages=[ "fastcord", "fastcord.utils", "fastcord.objects" ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=[ "websocket-client" ],
+    python_requires=">=3.6")

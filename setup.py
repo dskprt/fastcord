299a00aafa95db5a6f20da5002bc46a50f3a463c
dskprt <59660013+dskprt@users.noreply.github.com>
2020-03-28 11:47:55 +0100

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name="fastcord",
-    version="0.0.1",
+    version="0.1.0",
     description="another discord api wrapper for writing bots",
     author="dskprt",
     url="https://github.com/dskprt/fastcord",

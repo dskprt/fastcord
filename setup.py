2469989e8427a61ba1df7c4f1b0609c52450771e
dskprt <59660013+dskprt@users.noreply.github.com>
2020-04-06 17:30:59 +0200

    bump version to 0.2.0 & dev status to beta

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
 setuptools.setup(name="fastcord",
-    version="0.1.0",
+    version="0.2.0",
     description="another discord api wrapper for writing bots",
     author="dskprt",
     url="https://github.com/dskprt/fastcord",
     packages=[ "fastcord", "fastcord.utils", "fastcord.objects" ],
     classifiers = [
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent"

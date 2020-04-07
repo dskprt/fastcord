63211021fd554d65b80b02987ee9274a7ffaba48
dskprt <59660013+dskprt@users.noreply.github.com>
2020-04-07 11:44:30 +0200

    add imports

--- fastcord/__init__.py
+++ fastcord/__init__.py
@@ -1,4 +1,11 @@
 from .fastcord import Fastcord
-from .objects.embed import Embed
-from .objects.presence import Presence, Status
 from .objects.activity import Activity, ActivityType
+from .objects.attachment import Attachment
+from .objects.channel import Channel
+from .objects.embed import Embed
+from .objects.guild import Guild
+from .objects.message import Message
+from .objects.presence import Presence, Status
+from .objects.reaction import Reaction
+from .objects.role import Role
+from .objects.user import User
